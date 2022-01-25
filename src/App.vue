d5c0281195cddaeb65d489ca54ce322deadb9005
Hoyeoun Byun <jay08111@naver.com>
2022-01-25 22:41:01 +0900

    feat: init commit
    initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal
+      :header="header"
+      :text="text"
+      theme="sale"
+      :showModal="showModal"
+      @close="toggleModal"
+    />
+  </div>
+  <button @click="toggleModal">open modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "my first vue app !",
+      header: "sign up for giveaway",
+      text: "hello world",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="backdrop">
+    <div
+      class="modal"
+      :class="{ sale: theme === 'sale' }"
+      @click.self="closeModal"
+    >
+      <h1>{{ header }}</h1>
+      <p>modal content {{ text }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    header: {
+      type: String,
+    },
+    text: {
+      type: String,
+    },
+    theme: {
+      type: String,
+    },
+    showModal: {
+      type: Boolean,
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+<style>
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 10px;
+}
+.modal h1 {
+  color: #03fcb4;
+  border: none;
+  padding: 0;
+}
+.modal p {
+  font-style: normal;
+}
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h1 {
+  color: #fff;
+}
+.none {
+  display: none;
+}
+</style>
